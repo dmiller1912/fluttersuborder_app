0ba4362bb1d4fe178093493c21fb893944d0f59d
Dawson Miller <dmiller9@css.edu>
2021-05-09 09:28:26 -0500

    sprint 2 commit

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
 
   void _addSub() {
-
     showDialog(
         context: context,
         builder: (context) {
@@ -48,7 +47,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -76,6 +74,140 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+     class MyApp extends StatelessWidget {
+        // This widget is the root of your application.
+        @override
+        Widget build(BuildContext context) {
+          return MaterialApp(
+          title: 'FireStore Demo List',
+          theme: ThemeData(
+          primarySwatch: Colors.blue,
+            ),
+            home: FirebaseDemo(),
+               );
+            }
+         }
+
+     class FirebaseDemo extends StatefulWidget {
+      @override
+        _FirebaseDemoState createState() => _FirebaseDemoState();
+     }
+
+    class _FirebaseDemoState extends State<FirebaseDemo> {
+      final TextEditingController _newItemTextField = TextEditingController();
+        final CollectionReference itemCollectionDB = FirebaseFirestore.instance.collection('ITEMS');
+      List<String> itemList = [];
+
+    Widget nameTextFieldWidget() {
+      return SizedBox(
+        width: MediaQuery.of(context).size.width / 1.7,
+        child: TextField(
+        controller: _newItemTextField,
+        style: TextStyle(fontSize: 22, color: Colors.black),
+        decoration: InputDecoration(
+        hintText: "Item Name",
+        hintStyle: TextStyle(fontSize: 22, color: Colors.black),
+            ),
+          ),
+        );
+      }
+
+   Widget addButtonWidget() {
+      return SizedBox(
+      child: ElevatedButton(
+        onPressed: () async {
+          //setState(() {
+          //itemList.add(_newItemTextField.text);
+          //_newItemTextField.clear();
+        await itemCollectionDB.add({'item_name': _newItemTextField.text}).then((value) => _newItemTextField.clear());
+          //});
+            },
+        child: Text(
+        'Add Data',
+        style: TextStyle(fontSize: 20),
+              )),
+          );
+      }
+
+  Widget itemInputWidget() {
+      return Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+          nameTextFieldWidget(),
+            SizedBox(width: 10,),
+            addButtonWidget(),
+                ],
+            );
+          }
+
+  Widget itemTileWidget(snapshot, position) {
+    return ListTile(
+      leading: Icon(Icons.check_box),
+      title: Text(snapshot.data.docs[position]['item_name']),
+        onTap: () {
+          setState(() {
+              print("You tapped at postion =  $position");
+              String itemId = snapshot.data.docs[position].id;
+              itemCollectionDB.doc(itemId).delete();
+                  });
+              },
+          );
+      }
+
+  Widget itemListWidget() {
+      itemCollectionDB = FirebaseFirestore.instance.collection('USERS').doc(userID).collection('ITEMS');
+      return Expanded(
+      child:
+      StreamBuilder(stream: itemCollectionDB.snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+      return ListView.builder(
+        itemCount: snapshot.data.docs.length,
+          itemBuilder: (BuildContext context, int position) {
+          return Card(
+          child: itemTileWidget(snapshot,position)
+                               );
+                            }
+                      );
+                  })
+            );
+    }
+
+  @override
+  Widget build(BuildContext context) {
+      return Scaffold(
+        body: Container(
+         padding: EdgeInsets.symmetric(horizontal: 10, vertical: 50),
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+        child: Column(
+          children: [
+            itemInputWidget(),
+              SizedBox(height: 40,),
+                itemListWidget(),
+                            ],
+                          ),
+                    ),
+                );
+            }
+        }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
+        if(snapshot.hasData) {
+          print("data exists");
+          userID = FirebaseAuth.instance.currentUser.uid;
+        return mainScreen();
+      }
+      else {
+        return loginScreen();
+            }
+        },
+      );
+
+
 }
-
-
